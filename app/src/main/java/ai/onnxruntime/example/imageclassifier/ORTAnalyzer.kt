9f32e762f9cfb3c95791f0c2dbcdf36b6d4384b0
Guoyu Wang <wanggy@outlook.com>
2021-04-05 14:56:44 -0700

    Revert "completed app"
    This reverts commit 08a0d683d5ce991af65843f5723d841d69552173.

--- app/src/main/java/ai/onnxruntime/example/imageclassifier/ORTAnalyzer.kt
+++ app/src/main/java/ai/onnxruntime/example/imageclassifier/ORTAnalyzer.kt
@@ -1,6 +1,5 @@
 package ai.onnxruntime.example.imageclassifier
 
-import ai.onnxruntime.*
 import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.os.SystemClock
@@ -11,14 +10,14 @@ import kotlin.math.exp
 
 
 internal data class Result(
-    var detectedIndices: List<Int> = emptyList(),
-    var detectedScore: MutableList<Float> = mutableListOf<Float>(),
-    var processTimeMs: Long = 0
+        var detectedIndices: List<Int> = emptyList(),
+        var detectedScore: MutableList<Float> = mutableListOf<Float>(),
+        var processTimeMs: Long = 0
 ) {}
 
 internal class ORTAnalyzer(
-    private val ortSession: OrtSession?,
-    private val callBack: (Result) -> Unit
+        // TODO, add ORT to handel image analysis
+        private val callBack: (Result) -> Unit
 ) : ImageAnalysis.Analyzer {
 
     // Get index of top 3 values
@@ -78,26 +77,7 @@ internal class ORTAnalyzer(
         if (bitmap != null) {
             var result = Result()
 
-            val imgData = preProcess(bitmap)
-            val inputName = ortSession?.inputNames?.iterator()?.next()
-            val shape = longArrayOf(1, 3, 224, 224)
-            val env = OrtEnvironment.getEnvironment()
-            env.use {
-                val tensor = OnnxTensor.createTensor(env, imgData, shape)
-                val startTime = SystemClock.uptimeMillis()
-                tensor.use {
-                    val output = ortSession?.run(Collections.singletonMap(inputName, tensor))
-                    output.use {
-                        result.processTimeMs = SystemClock.uptimeMillis() - startTime
-                        val rawOutput = ((output?.get(0)?.value) as Array<FloatArray>)[0]
-                        val probabilities = softMax(rawOutput)
-                        result.detectedIndices = getTop3(probabilities)
-                        for (idx in result.detectedIndices) {
-                            result.detectedScore.add(probabilities[idx])
-                        }
-                    }
-                }
-            }
+            // TODO, add ORT inferencing code here
             callBack(result)
         }
 
@@ -106,6 +86,6 @@ internal class ORTAnalyzer(
 
     // We can switch analyzer in the app, need to make sure the native resources are freed
     protected fun finalize() {
-        ortSession?.close()
+        // TODO release native resources here
     }
 }
